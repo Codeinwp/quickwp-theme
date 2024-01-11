dec95a3a56ba4d3ff25b0e9259c6cad80c23d0af
John Fraskos <jfraskos@gmail.com>
2024-01-11 12:47:13 +0200

    Update team-columns.php

--- patterns/team-columns.php
+++ patterns/team-columns.php
@@ -8,10 +8,10 @@
 $quickwp_strings = apply_filters( 'quickwp_strings', array() );
 
 $images = array(
-	QUICKWP_URL . 'assets/images/qwp-images-05.webp',
-	QUICKWP_URL . 'assets/images/qwp-images-04.webp',
-	QUICKWP_URL . 'assets/images/qwp-images-03.webp',
-	QUICKWP_URL . 'assets/images/qwp-images-02.webp',
+	QUICKWP_URL . 'assets/images/qwp-img-05.webp',
+	QUICKWP_URL . 'assets/images/qwp-img-04.webp',
+	QUICKWP_URL . 'assets/images/qwp-img-03.webp',
+	QUICKWP_URL . 'assets/images/qwp-img-02.webp',
 );
 ?>
 
