16c1432ffa07059582cadd0dd63f5edf46449166
Hardeep Asrani <hardeepasrani@gmail.com>
2024-01-29 18:23:23 +0530

    experiment: content replace hooks

--- patterns/call-to-action-1.php
+++ patterns/call-to-action-1.php
@@ -17,11 +17,11 @@ $images = array(
 	<!-- wp:group {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"default"}} -->
 	<div class="wp-block-group alignwide">
 		<!-- wp:heading {"textAlign":"left","fontSize":"xx-large"} -->
-		<h2 class="wp-block-heading has-text-align-left has-xx-large-font-size"><?php echo esc_html( $quickwp_strings['quickwp_cta_title'] ); ?></h2>
+		<h2 class="wp-block-heading has-text-align-left has-xx-large-font-size"><?php echo esc_html( apply_filters( 'quickwp/call-to-action-1/title', $quickwp_strings['quickwp_cta_title'] ) ); ?></h2>
 		<!-- /wp:heading -->
 
 		<!-- wp:paragraph {"align":"left","fontSize":"large"} -->
-		<p class="has-text-align-left has-large-font-size"><?php echo esc_html( $quickwp_strings['feature_description'] ); ?></p>
+		<p class="has-text-align-left has-large-font-size"><?php echo esc_html( apply_filters( 'quickwp/call-to-action-1/subtitle', $quickwp_strings['feature_description'] ) ); ?></p>
 		<!-- /wp:paragraph -->
 	</div>
 	<!-- /wp:group -->
@@ -30,7 +30,7 @@ $images = array(
 	<div class="wp-block-buttons alignwide">
 		<!-- wp:button {"backgroundColor":"base-3","style":{"spacing":{"padding":{"left":"var:preset|spacing|60","right":"var:preset|spacing|60","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"className":"is-style-fill","fontSize":"large"} -->
 		<div class="wp-block-button has-custom-font-size is-style-fill has-large-font-size">
-			<a class="wp-block-button__link has-base-3-background-color has-background wp-element-button" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--60)"><?php echo esc_html( $quickwp_strings['button_text_2'] ); ?></a>
+			<a class="wp-block-button__link has-base-3-background-color has-background wp-element-button" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--60)"><?php echo esc_html( apply_filters( 'quickwp/call-to-action-1/button', $quickwp_strings['button_text_2'] ) ); ?></a>
 		</div>
 		<!-- /wp:button -->
 	</div>
--- patterns/feature-columns.php
+++ patterns/feature-columns.php
@@ -8,20 +8,20 @@
 $quickwp_strings = apply_filters( 'quickwp_strings', array() );
 
 $images = array(
-	QUICKWP_URL . 'assets/images/qwp-img-08.webp',
-	QUICKWP_URL . 'assets/images/qwp-img-06.webp',
-	QUICKWP_URL . 'assets/images/qwp-img-09.webp',
+	apply_filters( 'quickwp/feature-columns/item-1/image', QUICKWP_URL . 'assets/images/qwp-img-08.webp' ),
+	apply_filters( 'quickwp/feature-columns/item-2/image', QUICKWP_URL . 'assets/images/qwp-img-06.webp' ),
+	apply_filters( 'quickwp/feature-columns/item-3/image', QUICKWP_URL . 'assets/images/qwp-img-09.webp' )
 );
 ?>
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|40","margin":{"top":"0","bottom":"0"}}},"backgroundColor":"base-2","layout":{"type":"constrained"},"metadata":{}} -->
 <div class="wp-block-group alignfull has-base-2-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--40)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"constrained"},"metadata":{"name":"section title"}} -->
 <div class="wp-block-group"><!-- wp:heading {"textAlign":"center","fontSize":"x-large"} -->
-<h2 class="wp-block-heading has-text-align-center has-x-large-font-size"><?php echo esc_html( $quickwp_strings['feature_section_title'] ); ?></h2>
+<h2 class="wp-block-heading has-text-align-center has-x-large-font-size"><?php echo esc_html( apply_filters( 'quickwp/feature-columns/title', $quickwp_strings['feature_section_title'] ) ); ?></h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast-2"}}}},"textColor":"contrast-2","fontSize":"large"} -->
-<p class="has-text-align-center has-contrast-2-color has-text-color has-link-color has-large-font-size"><?php echo esc_html( $quickwp_strings['feature_description'] ); ?></p>
+<p class="has-text-align-center has-contrast-2-color has-text-color has-link-color has-large-font-size"><?php echo esc_html( apply_filters( 'quickwp/feature-columns/subtitle', $quickwp_strings['feature_description'] ) ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
@@ -35,17 +35,17 @@ $images = array(
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|30"}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo esc_html( $quickwp_strings['feature_title'] ); ?></h3>
+<h3 class="wp-block-heading"><?php echo esc_html( apply_filters( 'quickwp/feature-columns/item-1/title', $quickwp_strings['feature_title'] ) ); ?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo esc_html( $quickwp_strings['feature_description'] ); ?></p>
+<p><?php echo esc_html( apply_filters( 'quickwp/feature-columns/item-1/description', $quickwp_strings['feature_description'] ) ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_html( $quickwp_strings['button_text'] ); ?></a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_html( apply_filters( 'quickwp/feature-columns/item-1/button', $quickwp_strings['button_text'] ) ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
@@ -61,17 +61,17 @@ $images = array(
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|30"}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo esc_html( $quickwp_strings['feature_title'] ); ?></h3>
+<h3 class="wp-block-heading"><?php echo esc_html( apply_filters( 'quickwp/feature-columns/item-2/title', $quickwp_strings['feature_title'] ) ); ?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo esc_html( $quickwp_strings['feature_description'] ); ?></p>
+<p><?php echo esc_html( apply_filters( 'quickwp/feature-columns/item-2/description', $quickwp_strings['feature_description'] ) ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_html( $quickwp_strings['button_text'] ); ?></a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_html( apply_filters( 'quickwp/feature-columns/item-2/button', $quickwp_strings['button_text'] ) ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
@@ -87,17 +87,17 @@ $images = array(
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|30","right":"var:preset|spacing|30"},"blockGap":"var:preset|spacing|30"}},"backgroundColor":"base","layout":{"type":"constrained"}} -->
 <div class="wp-block-group has-base-background-color has-background" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group"><!-- wp:heading {"level":3} -->
-<h3 class="wp-block-heading"><?php echo esc_html( $quickwp_strings['feature_title'] ); ?></h3>
+<h3 class="wp-block-heading"><?php echo esc_html( apply_filters( 'quickwp/feature-columns/item-3/title', $quickwp_strings['feature_title'] ) ); ?></h3>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph -->
-<p><?php echo esc_html( $quickwp_strings['feature_description'] ); ?></p>
+<p><?php echo esc_html( apply_filters( 'quickwp/feature-columns/item-3/description', $quickwp_strings['feature_description'] ) ); ?></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_html( $quickwp_strings['button_text'] ); ?></a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button"><?php echo esc_html( apply_filters( 'quickwp/feature-columns/item-3/button', $quickwp_strings['button_text'] ) ); ?></a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>
